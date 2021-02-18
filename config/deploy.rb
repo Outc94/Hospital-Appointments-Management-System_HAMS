2e4c03cabd6f0c451809b162b816dfd1aefb386d
Outc94 <outc94@hotmail.fr>
2021-02-18 12:35:25 +0900

    Debugging AWS

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,43 +1,26 @@
 # config valid only for current version of Capistrano
+# config valid only for current version of Capistrano
 lock '3.6.0'
-
+# Application name to deploy
 set :application, 'hams'
+# git repository to clone
+# (Xxxxxxxx: user name, yyyyyyyy: application name)
 set :repo_url, 'https://github.com/Outc94/Hospital-Appointments-Management-System_HAMS'
+# The branch to deploy. The default is not necessarily master.
 set :branch, ENV['BRANCH'] || 'master'
+# The directory to deploy to.
 set :deploy_to, '/var/www/hams'
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
-
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-append :linked_files, 'config/database.yml', 'config/secrets.yml'
-
-# Default value for linked_dirs is []
-append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
+# Folders/files with symbolic links
+set :linked_files, %w{.env config/secrets.yml}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/uploads}
+# Number of versions to retain (*described later)
 set :keep_releases, 5
+# Ruby version
 set :rbenv_ruby, '2.6.5'
 set :rbenv_type, :system
-
+# The level of the log to output. Settings to: debug if you want to see the error log in detail.
+# For production environments,: info is normal。
+# However, if you want to check the behavior firmly, Settings it to: debug.
 set :log_level, :info
 namespace :deploy do
   desc 'Restart application'
